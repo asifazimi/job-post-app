9058d7613f65dbfe2ff3f70c15967efa34619962
asifazimi <asifazimi@outlook.com>
2024-07-15 17:03:20 +0500

    Add Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListingPosts from './components/JobListingPosts'
 
- const App = () => {
+const App = () => {
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Navbar/>       
+      <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs" />
+      <HomeCards/>
+      <JobListingPosts/>
+
+    <section className="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
+    
+    </>
   )
 }
 
-export default App;
+export default App
